bb79ca5379b402105f5e75bbeb79eb82bd981766
Luis Ricardo <luiscastrodev@gmail.com>
2023-08-29 12:47:43 -0300

    details screen

---
+++ app/src/main/java/pet/com/jetpetrescue/detail/Detail.kt
@@ -0,0 +1,73 @@
+package pet.com.jetpetrescue.detail
+
+import android.content.res.Configuration
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import hoods.com.jetpetrescue.ui.theme.JetPetTheme
+
+@Composable
+fun DetailScreen(
+    onNavigate: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Detail")
+                },
+                navigationIcon = {
+                    Icon(
+                        imageVector = Icons.Default.KeyboardArrowLeft,
+                        contentDescription = null,
+                        modifier =
+                        Modifier
+                            .size(36.dp, 36.dp)
+                            .clickable {
+                                onNavigate.invoke()
+                            },
+                        tint = MaterialTheme.colors.onSurface
+                    )
+                },
+                backgroundColor = MaterialTheme.colors.background,
+                contentColor = MaterialTheme.colors.onSurface,
+                elevation = 0.dp
+            )
+        }
+    ) { paddingValues ->
+        Column(modifier = Modifier.padding(paddingValues)) {
+
+        }
+    }
+}
+
+
+@Preview(
+    name = "Light mode",
+    showBackground = true
+)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true, name = "Dark Mode"
+)
+@Composable
+private fun HomePreview() {
+    JetPetTheme {
+        Surface {
+            DetailScreen(){}
+        }
+    }
+}
