41e74c5cc1075a8eb036a8e473ed7509f47ea2e8
Luis Ricardo <luiscastrodev@gmail.com>
2023-08-31 13:33:33 -0300

    state hosting

--- app/src/main/java/pet/com/jetpetrescue/MainActivity.kt
+++ app/src/main/java/pet/com/jetpetrescue/MainActivity.kt
@@ -6,6 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import hoods.com.jetpetrescue.ui.theme.JetPetTheme
 import pet.com.jetpetrescue.detail.DetailScreen
@@ -16,22 +20,49 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
+            var currentScreen by remember {
+                mutableStateOf(Screen.Home)
+            }
+
+            var seletedIndex by remember {
+                mutableStateOf(-1)
+            }
+
             JetPetTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Home(
-                        onPetClick = {
 
-                        },
-                        onSwitchClick = {
+                    when (currentScreen) {
+                        Screen.Home -> {
+                            Home(
+                                onPetClick = { index ->
+                                    seletedIndex = index
+                                    currentScreen = Screen.Detail
+                                },
+                                onSwitchClick = {
 
+                                }
+                            )
                         }
-                    )
+
+                        Screen.Detail -> {
+                            DetailScreen(index = seletedIndex) {
+                                currentScreen = Screen.Home
+                            }
+                        }
+
+                    }
+
                 }
             }
         }
     }
 }
 
+enum class Screen {
+    Home,
+    Detail
+}
